e422a64db6ec2c9dc7b06749fe6255e51f3836b2
Leonardo Villa <info@leographic.it>
2022-01-29 21:13:09 -0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,212 @@
+import './style.css'
+import * as dat from 'lil-gui'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+/**
+ * Base
+ */
+// Debug
+// const gui = new dat.GUI({
+//     width: 400
+// })
+
+var conf = {
+    color: "#121212"
+}
+
+var cameraCenter = new THREE.Vector3();
+var mouse = new THREE.Vector2();
+var cameraLimit = .1;
+
+// gui.addColor(conf, 'color').onChange(function(colorValue){
+//     scene.background.set(colorValue)
+// })
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('draco/')
+
+// GLTF loader
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+/**
+ * Texture
+ */
+
+const bakedTexture = textureLoader.load('desk-baked.jpg')
+bakedTexture.flipY = false
+bakedTexture.encoding = THREE.sRGBEncoding
+
+const groundBakedTexture = textureLoader.load('ground-baked.jpg')
+groundBakedTexture.flipY = false
+groundBakedTexture.encoding = THREE.sRGBEncoding
+
+/**
+ * Model
+ */
+
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
+
+const bakedgroundMaterial = new THREE.MeshBasicMaterial({ map: groundBakedTexture })
+
+gltfLoader.load('desk.glb', (gltf) => {
+    gltf.scene.traverse((child) =>
+    {
+        child.material = bakedMaterial
+    })
+    scene.add(gltf.scene)
+})
+
+gltfLoader.load('desk-ground.glb', (gltf) => {
+    gltf.scene.traverse((child) =>
+    {
+        child.material = bakedgroundMaterial
+    })
+    scene.add(gltf.scene)
+})
+
+/**
+ * Screen
+ */
+
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    
+    if (camera.aspect < 1.4 ) {
+        if (camera.aspect < 1.2){
+            if(camera.aspect < 0.7){
+                camera.fov = 90
+            }else{camera.fov = 60}
+        }else{camera.fov = 50}
+    } else {
+        camera.fov = 45
+    }
+
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 1000)
+
+if (camera.aspect < 1.4 ) {
+    if (camera.aspect < 1.2){
+        if(camera.aspect < 0.7){
+            camera.fov = 90
+        }else{camera.fov = 60}
+    }else{camera.fov = 50}
+} else {
+    camera.fov = 45
+}
+camera.updateProjectionMatrix()
+
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 3
+camera.lookAt(0,-0.2,0)
+scene.add(camera)
+
+cameraCenter.x = camera.position.x
+cameraCenter.y = camera.position.y
+cameraCenter.z = camera.position.z
+
+function updateCamera() {
+    //offset the camera x/y based on the mouse's position in the window
+    camera.position.y = cameraCenter.y + (cameraLimit * mouse.y);
+    camera.position.x = cameraCenter.x + (cameraLimit * mouse.x);
+
+    // if(mouse.x <= 0){
+    //     camera.position.x = cameraCenter.x + (cameraLimit * mouse.x);
+    // }else{
+    //     camera.position.z = cameraCenter.z - (cameraLimit * mouse.x);
+    // }
+
+    
+    camera.lookAt(0,-0.2,0)
+}
+
+function onDocumentMouseMove(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+// Controls
+//const controls = new OrbitControls(camera, canvas)
+//controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+scene.background = new THREE.Color(0x121212)
+
+renderer.outputEncoding = THREE.sRGBEncoding
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    //controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    updateCamera()
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
